f385ba830973b59e4e32bd5bdfa1bd15a9a1f9cf
zhouxiang1991 <zhouxiang219@163.com>
2020-04-13 16:23:15 +0800

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = {
 	rules: {
 		'body-leading-blank': [1, 'always'],
 		'footer-leading-blank': [1, 'always'],
-		'header-max-length': [2, 'always', 72],
+		'header-max-length': [2, 'always', 200],
 		'scope-case': [0, 'always', 'lower-case'],
 		'subject-case': [
 			0,
@@ -19,13 +19,14 @@ module.exports = {
       'always',
       [
         // 源码性修改
-        'feat', // 项目首次发版之后增加新功能
-        'fix', // 项目首次发版之后修复bug
-        'refactor', // 项目首次发版之后代码大范围变更即为重构
-        'pref', // 项目首次发版之后修改代码以提升性能
-        'demand', // 项目首次发版之后需求变更
-        'update', // 项目首次发版之前的代码变更, 未送测之前的代码变更都算
-        'style', // 不影响代码逻辑只影响代码外观的变更 例如：代码风格变更，空格，回车，注释，变量命名等
+        'feat', // 项目首次发版之后：增加新功能
+        'fix', // 项目首次发版之后：修复bug
+        'pref', // 项目首次发版之后：提升性能
+        'refactor', // 项目首次发版之后：重构代码
+        'user', // 项目首次发版后之后：提升用户体验
+        'demand', // 项目首次发版之后：需求变更
+        'dev', // 项目首次发版之前：未送测之前的代码变更
+        'style', // 不管何时不影响代码逻辑只影响代码外观的代码变更 例如：代码风格变更，空格，回车，注释，变量命名等
         // 非源码性修改
         'depend', // 项目依赖变更
         'docs', // 文档说明相关变更
