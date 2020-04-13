0a00ffa044e61760484bf3606ea3eddea77c7f57
zhouxiang1991 <zhouxiang219@163.com>
2020-04-13 16:48:13 +0800

    updat

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 module.exports = {
-	parserPreset: 'conventional-changelog-conventionalcommits',
-	rules: {
-		'body-leading-blank': [1, 'always'],
-		'footer-leading-blank': [1, 'always'],
-		'header-max-length': [2, 'always', 200],
-		'scope-case': [0, 'always', 'lower-case'],
-		'subject-case': [
-			0,
-			'never',
-			['sentence-case', 'start-case', 'pascal-case', 'upper-case']
-		],
-		'subject-empty': [0, 'never'],
-		'subject-full-stop': [2, 'never', '.'],
-		'type-case': [2, 'always', 'lower-case'],
-		'type-empty': [2, 'never'],
+  parserPreset: 'conventional-changelog-conventionalcommits',
+  rules: {
     'type-enum': [
       2,
       'always',
@@ -35,5 +22,5 @@ module.exports = {
         'chore', // 非源码性的其他杂项修改
       ],
     ],
-	}
+  }
 }
