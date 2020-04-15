ecf05177453936437fdb8099bf20e4f7db16cb35
zhouxiang1991 <zhouxiang219@163.com>
2020-04-15 14:53:31 +0800

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = {
   rules: {
     'body-leading-blank': [1, 'always'],
     'footer-leading-blank': [1, 'always'],
-    'header-max-length': [2, 'always', 72],
+    'header-max-length': [2, 'always', 300],
     'scope-case': [0, 'always', 'lower-case'],
     'subject-case': [
       0,
